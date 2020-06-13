4925b5a1c20aa22160633ff2a1796c868b9001dd
jadick <joaoatzdick@gmail.com>
2020-06-13 17:59:49 -0300

    update

--- compression.py
+++ compression.py
@@ -1,32 +1,9 @@
-""""        model1 = Full Resolution Image Compression with Recurrent Neural Networks 
-            model2 =   J. Ballé:
-                      "Efficient Nonlinear Transforms for Lossy Image Compression"
-                      Picture Coding Symposium (PCS), 2018
-                    
-                      b2018-leaky_relu-128-[1-4]
-                      b2018-leaky_relu-192-[1-4]
-                      b2018-gdn-128-[1-4]
-                      b2018-gdn-192-[1-4]
-  
-            model3 = J. Ballé, D. Minnen, S. Singh, S.J. Hwang, N. Johnston:
-                      "Variational Image Compression with a Scale Hyperprior"
-                      Int. Conf. on Learning Representations (ICLR), 2018
-                    
-                      bmshj2018-factorized-mse-[1-8]
-                      bmshj2018-factorized-msssim-[1-8]
-                      bmshj2018-hyperprior-mse-[1-8]
-                      bmshj2018-hyperprior-msssim-[1-8]
-            model4 =   D. Minnen, J. Ballé, G.D. Toderici:
-                      "Joint Autoregressive and Hierarchical Priors for Learned Image Compression"
-                      Adv. in Neural Information Processing Systems 31 (NeurIPS 2018)
-                    
-                      mbt2018-mean-mse-[1-8]
-                      mbt2018-mean-msssim-[1-8]
-            model5 = Learned Image Compression with Discretized Gaussian Mixture Likelihoods and Attention Modules
-"""""
 import argparse
 import textwrap
 import os
+import time
+import tensorflow as tf
+import pandas as pd
 
 
 def parse_arguments():
@@ -59,8 +36,36 @@ def parse_arguments():
                         )
     return parser.parse_args().paper
 
+def paper2():
+    models = ['b2018-leaky_relu-128-', 'b2018-leaky_relu-192-', 'b2018-gdn-128-', 'b2018-gdn-192-']
+    for model in models:
+        for quality in range(1, 5):
+            for image in range(1, 25):
+                print('################\nCOMPRESSING\nMODEL = ' + model + str(quality)+'\nIMAGE = '+ str(image) + '.png\n################')
+                t1 = time.perf_counter()
+                os.system('python modeltensorflow/tfci.py compress ' + model + str(quality) + ' images/' + str(image) + '.png')
+                t2 = time.perf_counter()
+                print('COMPRESSING TIME = ' + str(t2 - t1))
+                if not os.path.exists('modeltensorflow/Efficient_Nonlinear_Transforms_for_Lossy_Image_Compression/' + model[:-1] + '/' + model + str(quality) + '/'):
+                    os.makedirs('modeltensorflow/Efficient_Nonlinear_Transforms_for_Lossy_Image_Compression/' + model[:-1] + '/' + model + str(quality) + '/')
+                os.replace('images/' + str(image) + '.png.tfci','modeltensorflow/Efficient_Nonlinear_Transforms_for_Lossy_Image_Compression/' + model[:-1] + '/' + model + str(quality) + '/' + str(image) + '.tfci')
+                print('################\nDECOMPRESSING\nMODEL = ' + model + str(quality) + '\nIMAGE = ' + str(image) + '.png\n################')
+                t1 = time.perf_counter()
+                os.system('python modeltensorflow/tfci.py decompress modeltensorflow/Efficient_Nonlinear_Transforms_for_Lossy_Image_Compression/' + model[:-1] + '/' + model + str(quality) + '/' + str(image) + '.tfci')
+                t2 = time.perf_counter()
+                print('DECOMPRESSING TIME = ' + str(t2 - t1))
+
+
+def ms_ssim(path_original,path_reconstructed):
+    im1 = tf.decode_png(path_original)
+    im2 = tf.decode_png(path_reconstructed)
+    im1 = tf.image.convert_image_dtype(im1, tf.float32)
+    im2 = tf.image.convert_image_dtype(im2, tf.float32)
+    psnr = tf.image.psnr(im1, im2, max_val=1.0)
+    print(psnr)
+
+
 def main(papers):
-    model2 = ['b2018-leaky_relu-128-','b2018-leaky_relu-192-','b2018-gdn-128-','b2018-gdn-192-']
     model3 = ['bmshj2018-factorized-mse-','bmshj2018-factorized-msssim-','bmshj2018-hyperprior-mse-','bmshj2018-hyperprior-msssim-']
     model4 = ['mbt2018-mean-mse-','mbt2018-mean-msssim-']
     for paper in papers:
@@ -75,11 +80,7 @@ def main(papers):
             print('##########################################################')
             print('Efficient Nonlinear Transforms for Lossy Image Compression')
             print('##########################################################')
-            for version in model2:
-                for quality in range(1,5):
-                    for image in range(1,25):
-                        os.system('python modeltensorflow/tfci.py compress'+version+str(quality)+'modeltensorflow/'+str(image)+'.png')
-
+            paper2()
         elif paper == '3':
             #os.system('conda deactivate')
             #os.system('conda activate modelgoogle')
@@ -87,8 +88,8 @@ def main(papers):
             print('Variational Image Compression with a Scale Hyperprior')
             print('#####################################################')
             for image in range(1,25):
-                os.system('python model2-3/tfci.py compress bmshj2018-hyperprior-msssim-4 model2-3/'+str(image)+'.png')
-                os.system('python model2-3/tfci.py decompress model2-3/'+str(image)+'.png.tfci ')
+                os.system('python modeltesorflow/tfci.py compress bmshj2018-hyperprior-msssim-4 modeltensorflow/'+str(image)+'.png')
+                os.system('python modeltensorflow/tfci.py decompress model2-3/'+str(image)+'.png.tfci ')
             #os.system('conda deactivate')
         elif paper == '5':
             print('#############################################################################################')
